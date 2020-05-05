98f4048b863bf6788ea75c0df83948222a423422
HwanChang <ghksckd219@gmail.com>
2020-05-05 18:32:41 +0900

    LeetCode 2020년 05월 05일 해결

---
+++ src/com/leetcode/may/May05.java
@@ -0,0 +1,31 @@
+package com.leetcode.may;
+
+import java.util.HashMap;
+
+public class May05 {
+
+    // Runtime: 22 ms
+    // Memory Usage: 39.6 MB
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        char[] charArr = s.toCharArray();
+
+        for(char c : charArr) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : charArr) {
+            if(map.get(c) == 1) {
+                return s.indexOf(c);
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        int answer = firstUniqChar(s);
+        System.out.println(answer);
+    }
+}
