8f58fe70a8addde250e7222fb100f7607dbcd45b
HwanChang <ghksckd219@gmail.com>
2020-05-18 21:18:40 +0900

    LeetCode 2020년 05월 18일 해결
    문자열 관련 문제

---
+++ src/com/leetcode/may/Permutation_in_String.java
@@ -0,0 +1,42 @@
+package com.leetcode.may;
+
+import java.util.Arrays;
+
+public class Permutation_in_String {
+
+    public static boolean checkInclusion(String s1, String s2) {
+        int[] s1Array = new int[26];
+        int[] s2Array = new int[26];
+
+        for(char c: s1.toCharArray()) {
+            s1Array[c - 'a']++;
+        }
+        
+        int n = s1.length();
+
+        for(int i=0; i<s2.length(); i++){
+            char c = s2.charAt(i);
+
+            if (i>=n){
+                s2Array[s2.charAt(i-n) - 'a']--;
+            }
+
+            s2Array[c - 'a']++;
+            if(Arrays.equals(s2Array, s1Array)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+//        String s1 = "ab";
+//        String s2 = "eidboaoo";
+
+        boolean answer = checkInclusion(s1, s2);
+        System.out.println(answer);
+    }
+}
